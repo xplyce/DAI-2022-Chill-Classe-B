8a170b56c8a920401833e2ff2a6e893560872f13
wiZZard <pismice15@gmail.com>
2022-09-22 11:46:21 +0200

    ajout du boxer

---
+++ src/test/java/ch/heigvd/dai/chill/domain/santoro/BoxerTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.dai.chill.domain.santoro;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BoxerTest {
+
+    @Test
+    void thePriceAndNameForPunkIPAShouldBeCorrect() {
+        Boxer beer = new Boxer();
+        assertEquals(beer.getName(), Boxer.NAME);
+        assertEquals(beer.getPrice(), Boxer.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForBoxer() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.dai.chill.domain.santoro.Boxer";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Boxer.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
